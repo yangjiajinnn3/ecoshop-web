b238161494e1415b2691f610ceccd56e040c0a1a
yangjia <yjin@insmollerussa.cat>
2026-04-10 11:18:09 +0200

    afegir models MongoDB per a productes

---
+++ src/models/Productes.js
@@ -0,0 +1,10 @@
+const productesSchema = {
+nom: String,
+preu: Number,
+categoria: String,
+estoc: Number,
+imatgeUrl: String,
+altText: String,
+certificacioEcologica: Boolean
+};
+module.exports = productesSchema;
